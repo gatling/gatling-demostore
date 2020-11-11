be3152fb8b48ca2a0b1f0f8c45a389a9e74e668b
James Willett <james.willett@sap.com>
2020-11-11 15:11:28 +0000

    Adding HTTPS Some more cleanup to do of these files

--- src/main/java/com/gatling/demostore/WebConfig.java
+++ src/main/java/com/gatling/demostore/WebConfig.java
@@ -1,5 +1,12 @@
 package com.gatling.demostore;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 // import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -19,6 +26,34 @@ public class WebConfig implements WebMvcConfigurer{
         registry
         .addResourceHandler("/resources/**")
         .addResourceLocations("classpath:/media/");
+    }
+
+
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(getHttpConnector());
+        return tomcat;
+    }
+
+    private Connector getHttpConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+        return connector;
     }
     
 }
--- src/main/java/com/gatling/demostore/security/SecurityConfig.java
+++ src/main/java/com/gatling/demostore/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -29,22 +31,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-        .authorizeRequests()
-        .antMatchers("/category/**").hasAnyRole("USER", "ADMIN")
-        .antMatchers("/admin/**").hasAnyRole("ADMIN")
-        .antMatchers("/").permitAll()
-            .and()
-                .formLogin()
-                    .loginPage("/login")
-            .and()
-                .logout()
-                    .logoutSuccessUrl("/")
-            .and()
-                .exceptionHandling()
-                    .accessDeniedPage("/");
+    // @Override
+    // protected void configure(HttpSecurity http) throws Exception {
+    //     http
+    //     .authorizeRequests()
+    //     .antMatchers("/category/**").hasAnyRole("USER", "ADMIN")
+    //     .antMatchers("/admin/**").hasAnyRole("ADMIN")
+    //     .antMatchers("/").permitAll()
+    //         .and()
+    //             .formLogin()
+    //                 .loginPage("/login")
+    //         .and()
+    //             .logout()
+    //                 .logoutSuccessUrl("/")
+    //         .and()
+    //             .exceptionHandling()
+    //                 .accessDeniedPage("/");
 
  //       .antMatchers("/**").hasAnyAuthority("USER");
 
@@ -53,6 +55,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         //     .authorizeRequests()
         //         .antMatchers("/category/**").access("hasRole('ROLE_USER')")
         //         .antMatchers("/").access("permitAll");
+ //   }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+         http
+            .requiresChannel()
+            .anyRequest()
+            .requiresSecure()
+        .and()
+            .authorizeRequests()
+            .antMatchers("/**")
+            .permitAll();
     }
     
 }
