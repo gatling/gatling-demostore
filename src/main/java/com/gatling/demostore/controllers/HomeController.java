a12e6feeb2611bd9a09a2bcbda0e61c3f1d3def2
James Willett <james.willett@sap.com>
2020-11-17 07:38:52 +0000

    Return empty favicon

--- src/main/java/com/gatling/demostore/controllers/HomeController.java
+++ src/main/java/com/gatling/demostore/controllers/HomeController.java
@@ -2,6 +2,11 @@ package com.gatling.demostore.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class HomeController {
@@ -11,9 +16,5 @@ public class HomeController {
         return "home";
     }
 
-    // @GetMapping("/checkout")
-    // public String checkoutPage() {
-    //     return "checkout";
-    // }
-    
 }
+
