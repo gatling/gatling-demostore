1690dc9ebc0d97bb6c0213b94a74209d6cc407aa
Stephane Landelle <slandelle@gatling.io>
2023-02-14 12:24:58 +0100

    chore: remove dead code
    Motivation:
    
    We have no url n `/resources/**`

--- src/main/java/io/gatling/demostore/WebConfig.java
+++ src/main/java/io/gatling/demostore/WebConfig.java
@@ -2,19 +2,10 @@ package io.gatling.demostore;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/resources/**")
-                .addResourceLocations("classpath:/media/")
-                .addResourceLocations("classpath:/downloads/");
-    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
